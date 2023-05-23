28671b681fb43fefa96b12c8ebc07c2be8fdfb26
Vicky John <vicky.d.john@email.com>
2023-05-22 18:33:43 -0700

    minor changes to improve readbility

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const { fetchMyIp } = require('./iss');
+const { fetchMyIp, fetchCoordsByIp } = require('./iss');
 
 // fetchMyIp((error, ip) => {
 //   if (error) {
@@ -8,3 +8,12 @@ const { fetchMyIp } = require('./iss');
 
 //   console.log('It worked! Returned IP: ', ip);
 // });
+
+fetchCoordsByIp('162.245.144.188', (error, coordinates) => {
+  if (error) {
+    console.log("It didn't work!" , error);
+    return;
+  }
+
+  console.log('It worked! Returned coordinates:' , coordinates);
+});
--- iss.js
+++ iss.js
@@ -1,19 +1,37 @@
 const request = require('request');
 
 const ipUrl = 'https://api64.ipify.org/?format=json';
+const coordUrl = 'http://ipwho.is/174.4.104.208';
 
 exports.fetchMyIp = (callback) => {
   request(ipUrl, (error, response, body) => {
-    console.log(body)
     if (error) {
       return callback(error);
     }
     if (response.statusCode !== 200) {
-      callback(Error(`Request failed with status code: ${response.statusCode}`), null)
-      return
+      callback(Error(`Request failed with status code: ${response.statusCode}`), null);
+      return;
     }
 
     const ip = JSON.parse(body);
-    callback(null, ip)
+    callback(null, ip);
+  });
+};
+
+exports.fetchCoordsByIp = (ip, callback) => {
+  request(coordUrl, (error, response, body) => {
+    if (error) {
+      callback(error, null);
+      return;
+    }
+    const coord = JSON.parse(body);
+    if (!coord.success) {
+      const message = `Success status was ${coord.success}. Server message says: ${coord.message} when fetching for IP ${coord.ip}`;
+      callback(Error(message), null);
+      return;
+    } 
+
+    const coordinates = { latitude: coord.latitude, longitude: coord.longitude };
+    callback(null, coordinates);
   });
 };
